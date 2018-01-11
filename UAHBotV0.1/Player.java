524073e8bf4abff1aaf5b065e214dc7b63b2ea62
Jedimaster4559 <nbsolomon4559@gmail.com>
2018-01-10 22:03:00 -0600

    Added basis of code for Player

--- UAHBotV0.1/Player.java
+++ UAHBotV0.1/Player.java
@@ -2,6 +2,19 @@ import bc.*;
 
 public class Player {
   public static void main(String[] args) {
-  
+	
+	//Add a Game Controller
+	GameController gc = new GameController();
+	
+	while (true){
+		System.Out.Println("CurrentRound: " + gc.round());
+		
+		VecUnit units = gc.myUnits();
+		for (int i = 0; i < units.size(); i++) {
+			Unit unit = units.get(i);
+        }
+		gc.nextTurn();
+	}
+	
   }
 }
