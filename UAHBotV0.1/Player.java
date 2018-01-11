de4b49056bfd4ac4088c327c4f959a04c616cef1
Jedimaster4559 <nbsolomon4559@gmail.com>
2018-01-10 23:05:36 -0600

    Added lgic to run through all unit types

--- UAHBotV0.1/Player.java
+++ UAHBotV0.1/Player.java
@@ -12,9 +12,55 @@ public class Player {
 		VecUnit units = gc.myUnits();
 		for (int i = 0; i < units.size(); i++) {
 			Unit unit = units.get(i);
+			
+			
+			//This block determines unit type and then executes a method accordingly
+			if(unit.unitType() == UnitType.Factory)
+				runFactory(unit);
+			if(unit.unitType() == UnitType.Worker)
+				runWorker(unit);
+			if(unit.unitType() == UnitType.Knight)
+				runKnight(unit);
+			if(unit.unitType() == UnitType.Mage)
+				runMage(unit);
+			if(unit.unitType() == UnitType.Ranger)
+				runRanger(unit);
+			if(unit.unitType() == UnitType.Healer)
+				runHealer(unit);
+			if(unit.unitType() == UnitType.Rocket)
+				runRocket(unit);
+			
         }
 		gc.nextTurn();
 	}
 	
+  }
+  
+  static void runFactory(Unit unit){
+	  
+  }
+  
+  static void runWorker(Unit unit){
+	  
+  }
+  
+  static void runKnight(Unit unit){
+	  
+  }
+  
+  static void runMage(Unit unit){
+	  
+  }
+  
+  static void runRanger(Unit unit){
+	  
+  }
+  
+  static void runHealer(Unit unit){
+	  
+  }
+  
+  static void runRocket(Unit unit){
+	  
   }
 }
