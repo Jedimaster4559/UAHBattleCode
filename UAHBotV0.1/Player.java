bfc822a2bf7cfdff567c45e43df5c2ca634743d6
GoldenPhysics <goldenphysicsroblox@gmail.com>
2018-01-11 23:38:40 -0600

    Indentation fix
    Fixes indentation only. No other code changes.

--- UAHBotV0.1/Player.java
+++ UAHBotV0.1/Player.java
@@ -16,100 +16,100 @@ public class Player {
 		
 	public static void main(String[] args) {
 	
-	//Add a Game Controller
-	gc = new GameController();
-	
-	//Seed Randomizer for debugging purposes
-	rand.setSeed(4559);
-	
-	//Create and Array of all Directions a bot can travel
-	directions = Direction.values();
-	
-	while (true){
-		System.out.println("CurrentRound: " + gc.round());
+		//Add a Game Controller
+		gc = new GameController();
 		
-		VecUnit units = gc.myUnits();
-		for (int i = 0; i < units.size(); i++) {
-			Unit unit = units.get(i);
+		//Seed Randomizer for debugging purposes
+		rand.setSeed(4559);
+		
+		//Create and Array of all Directions a bot can travel
+		directions = Direction.values();
+		
+		while (true){
+			System.out.println("CurrentRound: " + gc.round());
 			
-			
-			//This block determines unit type and then executes a method accordingly
-			//RunFactory
-			if(unit.unitType() == UnitType.Factory){
-				try{
-					runFactory(unit);
+			VecUnit units = gc.myUnits();
+			for (int i = 0; i < units.size(); i++) {
+				Unit unit = units.get(i);
+				
+				
+				//This block determines unit type and then executes a method accordingly
+				//RunFactory
+				if(unit.unitType() == UnitType.Factory){
+					try{
+						runFactory(unit);
+					}
+					catch(Exception e){
+						System.out.println("A Factory Error Occurred:\nUnit Id: " + unit.id());
+						e.printStackTrace();
+					}
 				}
-				catch(Exception e){
-					System.out.println("A Factory Error Occurred:\nUnit Id: " + unit.id());
-					e.printStackTrace();
+				//Run Worker
+				if(unit.unitType() == UnitType.Worker){
+					try{
+						runWorker(unit);
+					}
+					catch(Exception e) {
+						System.out.println("A Worker Error Occurred:\nUnit Id: " + unit.id());
+						e.printStackTrace();
+					}
 				}
+				//Run Knight
+				if(unit.unitType() == UnitType.Knight){
+					try{
+						runKnight(unit);
+					}
+					catch(Exception e) {
+						System.out.println("A Knight Error Occurred:\nUnit Id: " + unit.id());
+						e.printStackTrace();
+					}
+				}
+				//Run Mage
+				if(unit.unitType() == UnitType.Mage){
+					try{
+						runMage(unit);
+					}
+					catch(Exception e) {
+						System.out.println("A Mage Error Occurred:\nUnit Id: " + unit.id());
+						e.printStackTrace();
+					}
+				}
+				//Run Ranger
+				if(unit.unitType() == UnitType.Ranger){
+					try{
+						runRanger(unit);
+					}
+					catch(Exception e) {
+						System.out.println("A Ranger Error Occurred:\nUnit Id: " + unit.id());
+						e.printStackTrace();
+					}
+				}
+				//Run Healer
+				if(unit.unitType() == UnitType.Healer){
+					try{
+						runHealer(unit);
+					}
+					catch(Exception e) {
+						System.out.println("A Healer Error Occurred:\nUnit Id: " + unit.id());
+						e.printStackTrace();
+					}
+				}
+				//Run Rocket
+				if(unit.unitType() == UnitType.Rocket){
+					try{
+						runRocket(unit);
+					}
+					catch(Exception e) {
+						System.out.println("A Rocket Error Occurred:\nUnit Id: " + unit.id());
+						e.printStackTrace();
+					}
+				}
+				
 			}
-			//Run Worker
-			if(unit.unitType() == UnitType.Worker){
-				try{
-					runWorker(unit);
-				}
-				catch(Exception e) {
-					System.out.println("A Worker Error Occurred:\nUnit Id: " + unit.id());
-					e.printStackTrace();
-				}
-			}
-			//Run Knight
-			if(unit.unitType() == UnitType.Knight){
-				try{
-					runKnight(unit);
-				}
-				catch(Exception e) {
-					System.out.println("A Knight Error Occurred:\nUnit Id: " + unit.id());
-					e.printStackTrace();
-				}
-			}
-			//Run Mage
-			if(unit.unitType() == UnitType.Mage){
-				try{
-					runMage(unit);
-				}
-				catch(Exception e) {
-					System.out.println("A Mage Error Occurred:\nUnit Id: " + unit.id());
-					e.printStackTrace();
-				}
-			}
-			//Run Ranger
-			if(unit.unitType() == UnitType.Ranger){
-				try{
-					runRanger(unit);
-				}
-				catch(Exception e) {
-					System.out.println("A Ranger Error Occurred:\nUnit Id: " + unit.id());
-					e.printStackTrace();
-				}
-			}
-			//Run Healer
-			if(unit.unitType() == UnitType.Healer){
-				try{
-					runHealer(unit);
-				}
-				catch(Exception e) {
-					System.out.println("A Healer Error Occurred:\nUnit Id: " + unit.id());
-					e.printStackTrace();
-				}
-			}
-			//Run Rocket
-			if(unit.unitType() == UnitType.Rocket){
-				try{
-					runRocket(unit);
-				}
-				catch(Exception e) {
-					System.out.println("A Rocket Error Occurred:\nUnit Id: " + unit.id());
-					e.printStackTrace();
-				}
-			}
-			
-        }
-		gc.nextTurn();
-	}
+			gc.nextTurn();
+		}
 	
-  }
+	}
   
 	static void runFactory(Unit unit){
 	  
