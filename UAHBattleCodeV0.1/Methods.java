4b842bdbfa1dedc5edd6b5b09f61c19815085d2d
Jedimaster4559 <nbsolomon4559@gmail.com>
2018-01-11 22:05:26 -0600

    Added Methods file with useful methods

---
+++ UAHBattleCodeV0.1/Methods.java
@@ -0,0 +1,38 @@
+import bc.*;
+import java.util.*;
+
+public class Methods{
+	static void countUnits(VecUnit units){
+	  for(int = 0; i < unit.size(); i++){
+		  numFactories = 0;
+		  numWorkers = 0;
+		  numKnights = 0;
+		  numMages = 0;
+		  numRangers = 0;
+		  numHealers = 0
+		  numRockets = 0;
+		  if(unit.unitType() == UnitType.Factory)
+			numFactories++;
+		  if(unit.unitType() == UnitType.Worker)
+			numWorkers++;
+		  if(unit.unitType() == UnitType.Knight)
+			numKnights++;
+		  if(unit.unitType() == UnitType.Mage)
+			numMages++;
+		  if(unit.unitType() == UnitType.Ranger)
+			numRangers++;
+		  if(unit.unitType() == UnitType.Healer)
+			numHealers++;
+		  if(unit.unitType() == UnitType.Rocket)
+			numRockets++;
+	  }
+  }
+  
+  static void moveRandomDirection(Unit unit){
+	  Direction randomDirection = directions[rand.nextInt(directions.length)];
+	  if(gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), directions[rand.nextInt(directions.length)])){
+		  gc.moveRobot(unit.id(), randomDirection);
+	  }
+  }
+	
+}
