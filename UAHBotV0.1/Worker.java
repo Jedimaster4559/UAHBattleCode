5d5d062bd24d01535be1158debad6da7925a6df6
GoldenPhysics <goldenphysicsroblox@gmail.com>
2018-01-12 17:32:27 -0600

    Add commented build stuff`
    Now the worker can build, once it's uncommented.

--- UAHBotV0.1/Worker.java
+++ UAHBotV0.1/Worker.java
@@ -18,7 +18,13 @@ class Worker {
 			System.out.println("Harvesting!");
 			gc.harvest(unit.id(), Direction.Center);
 		//} else if (gc.canBuild(unit.id()) {
-		//	gc.Build(unit.id());
+		//	VecUnit factories = gc.senseUnitAtLocation(
+		//			unit.location().mapLocation(), 50, UnitType.Factory);
+		//	for (int i = 0; i < factories.size(); i++) {
+		//		if (gc.canBuild(unit.id(), factories.get(i))) {
+		//			gc.build(unit.id(), factories.get(i));
+		//		}
+		//	}
 		} else if (gc.canBlueprint(unit.id(), UnitType.Factory, Direction.Center)) {
 			System.out.println("Blueprinting!");
 			gc.blueprint(unit.id(), UnitType.Factory, Direction.Center);
