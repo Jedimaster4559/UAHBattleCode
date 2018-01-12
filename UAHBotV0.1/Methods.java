921a14f9f1f9dee8e9d4d5addaa9b26d321f6d29
Jedimaster4559 <nbsolomon4559@gmail.com>
2018-01-12 16:40:46 -0600

    Moved all methods in methods.java to utilities.java as these are utility methods

--- UAHBotV0.1/Methods.java
+++ UAHBotV0.1/Methods.java
@@ -2,37 +2,6 @@ import bc.*;
 import java.util.*;
 
 public class Methods{
-	public static void countUnits(VecUnit units){
-		Player.numFactories = 0;
-		Player.numWorkers = 0;
-		Player.numKnights = 0;
-		Player.numMages = 0;
-		Player.numRangers = 0;
-		Player.numHealers = 0
-		Player.numRockets = 0;
-		for(int = 0; i < unit.size(); i++){
-			if(unit.unitType() == UnitType.Factory)
-				numFactories++;
-			if(unit.unitType() == UnitType.Worker)
-				numWorkers++;
-			if(unit.unitType() == UnitType.Knight)
-				numKnights++;
-			if(unit.unitType() == UnitType.Mage)
-				numMages++;
-			if(unit.unitType() == UnitType.Ranger)
-				numRangers++;
-			if(unit.unitType() == UnitType.Healer)
-				numHealers++;
-			if(unit.unitType() == UnitType.Rocket)
-				numRockets++;
-		}
-	}
-  
-	public static void moveRandomDirection(Unit unit){
-		Direction randomDirection = directions[rand.nextInt(directions.length)];
-		if(gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), directions[rand.nextInt(directions.length)])){
-			gc.moveRobot(unit.id(), randomDirection);
-		}
-	}
+	
 	
 }
