04c6ac064a2e1bda57f1d39c82e3f047f6afbecd
GoldenPhysics <goldenphysicsroblox@gmail.com>
2018-01-11 23:49:37 -0600

    More indentation fixes
    More indentation fixes. Just removed a level. Weird diffs here, though.

--- UAHBotV0.1/Factory.java
+++ UAHBotV0.1/Factory.java
@@ -2,36 +2,37 @@ import bc.*;
 
 class Factory {
 		
-		public static boolean canProcess(Unit unit){
-			if (unit.unitType() == UnitType.Factory){
-				return true;
-			}
-			return false;
+	public static boolean canProcess(Unit unit){
+		if (unit.unitType() == UnitType.Factory){
+			return true;
 		}
+		return false;
+	}
 
-		public static void process(Unit unit, GameController gc){
-			//Test to make sure the factory exists in a valid location
-			if (unit.location().mapLocation().getPlanet() == null) {
-				return;
+	public static void process(Unit unit, GameController gc){
+		//Test to make sure the factory exists in a valid location
+		if (unit.location().mapLocation().getPlanet() == null) {
+			return;
+		}
+		System.out.println("Valid Location");
+		
+		//Creates a new unit if the factory isn't producing
+		if(!unit.isFactoryProducing()){
+			UnitType unitCreateType = decideUnitType();
+			if(gc.canProduceRobot(unit.id(), unitCreateType)){
+				gc.produceRobot(unit.id(), unitCreateType);
 			}
-			System.out.println("Valid Location");
-			
-			//Creates a new unit if the factory isn't producing
-			if(!unit.isFactoryProducing()){
-				UnitType unitCreateType = decideUnitType();
-				if(gc.canProduceRobot(unit.id(), unitCreateType)){
-					gc.produceRobot(unit.id(), unitCreateType);
-				}
-			}
-			
-			
-			
 		}
 		
-		public static UnitType decideUnitType(){
-			if(true){
-				return UnitType.Worker;
-			}
-			
+		
+		
+	}
+	
+	public static UnitType decideUnitType(){
+		
+		if(true){
+			return UnitType.Worker;
 		}
+		
+	}
 }
