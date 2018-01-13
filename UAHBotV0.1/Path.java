3d917ab07b92c1abcc97542331a3e534b9d3bd99
Jedimaster4559 <nbsolomon4559@gmail.com>
2018-01-13 17:24:18 -0600

    added fuzzyPath(Unit unit, MapLocation dest, GameController gc) pathing method

--- UAHBotV0.1/Path.java
+++ UAHBotV0.1/Path.java
@@ -1,5 +1,36 @@
 import bc.*;
 
 class Path {
+	static PlanetMap earth;
+	static PlanetMap mars;
+	static int mapHeight;
+	static int mapLength;
+	static Direction[] directions;
+	static int[] tryRotate;
+	
+	static void initializePathing(GameController gc){
+		earth = gc.startingMap(Planet.Earth);
+		mars = gc.startingMap(Planet.Mars);
+		directions = new Direction[]{Direction.North, Direction.Northeast, Direction.East, Direction.Southeast, Direction.South, Direction.Southwest, Direction.West, Direction.Northwest};
+		tryRotate = new int[]{0, -1, 1, -2, 2};
+	}
+	
+	static void fuzzyPath(Unit unit, MapLocation dest, GameController gc){
+		MapLocation start = unit.location().mapLocation();
+		Direction toward = start.directionTo(dest);
+		for(int tilt:tryRotate){
+			Direction currentDirection = rotate(toward, tilt, gc);
+			if(gc.canMove(unit.id(), currentDirection)){
+				gc.moveRobot(unit.id(), currentDirection);
+			}
+		}
+	}
+	
+	static Direction rotate(Direction dir, int amount, GameController gc){
+		int index = Utilities.getIndex(directions, dir);
+		return directions[(index+amount)%8];
+	}
+	
+	
 	
 }
