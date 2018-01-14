04e5985ed897ad9f097f84a7097cb561bff7bd0d
Jedimaster4559 <nbsolomon4559@gmail.com>
2018-01-13 18:19:38 -0600

    added bugPath(Unit unit, MapLocation dest, GameController gc) pathing method

--- UAHBotV0.1/Path.java
+++ UAHBotV0.1/Path.java
@@ -15,6 +15,8 @@ class Path {
 		tryRotate = new int[]{0, -1, 1, -2, 2};
 	}
 	
+	//Fuzzy Path attempts to take the shortest path and can get around really
+	//simple obstacles. It is really low processing but will occasionally fail
 	static void fuzzyPath(Unit unit, MapLocation dest, GameController gc){
 		MapLocation start = unit.location().mapLocation();
 		Direction toward = start.directionTo(dest);
@@ -22,13 +24,38 @@ class Path {
 			Direction currentDirection = rotate(toward, tilt, gc);
 			if(gc.canMove(unit.id(), currentDirection)){
 				gc.moveRobot(unit.id(), currentDirection);
+				break;
 			}
 		}
 	}
 	
+	//Method to take a direction and rotate it by a certain amount.
+	//Positive amounts rotate clockwise and negative rotate counter clockwise
 	static Direction rotate(Direction dir, int amount, GameController gc){
 		int index = Utilities.getIndex(directions, dir);
 		return directions[(index+amount)%8];
+	}
+	
+	//Bug path is a much more accurate method of going to a destination
+	//but has the problem that it may not be the most intelligent. In most
+	//cases, however, this will be the best method due to a combination of
+	//its simplicity, and it's accuracy.
+	static void bugPath(Unit unit, MapLocation dest, GameController gc){
+		MapLocation start = unit.location().mapLocation();
+		Direction toward = start.directionTo(dest);
+		if(gc.canMove(unit.id(), toward)){
+			gc.moveRobot(unit.id(), toward);
+		} else {
+			for(int i = 0; i<8; i++){
+				toward = rotate(toward, 1, gc);
+				if(gc.canMove(unit.id(), toward)){
+					gc.moveRobot(unit.id(), toward);
+					break;
+				}
+			}
+		}
+			
+		
 	}
 	
 	
