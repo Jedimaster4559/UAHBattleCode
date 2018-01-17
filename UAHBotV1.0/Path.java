95efac0b00f085b7ebc469c65b8e7e571e8e9049
GoldenPhysics <goldenphysicsroblox@gmail.com>
2018-01-17 17:26:59 -0600

    Comment out unused statement

--- UAHBotV1.0/Path.java
+++ UAHBotV1.0/Path.java
@@ -129,7 +129,7 @@ class Path {
 	//Method designed to be used to determine and run the best pathing algorithm
 	//Should be used in the case where you
 	static void determinePathing(Unit unit, MapLocation dest, GameController gc){
-		UnitType type = unit.unitType();
+		//UnitType type = unit.unitType();
 		bugPath(unit, dest, gc);
 		
 	}
