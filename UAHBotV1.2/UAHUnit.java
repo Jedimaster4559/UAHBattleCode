a92446c2f66eae34b52d811f1159bf5979809cfe
GoldenCompSci <sam.lukins.compsci@gmail.com>
2018-01-21 18:31:47 -0600

    Update UAHUnit to include GameController

--- UAHBotV1.2/UAHUnit.java
+++ UAHBotV1.2/UAHUnit.java
@@ -2,16 +2,18 @@ import bc.*;
 
 public abstract class UAHUnit {
 	
+	private GameController gc;
 	private Unit unit;
 	private int unitId;
 	private MapLocation currentLocation;
 	private MapLocation dest;
 	
-	public UAHUnit(Unit unit) {
+	public UAHUnit(Unit unit, GameController gc) {
 		this.unit = unit;
 		unitId = unit.id();
 		currentLocation = unit.location().mapLocation();
 		dest = null;
+		this.gc = gc;
 	}
 	
 	public Unit getUnit() {
