7c35ecfe36e47a8562dcf12ce9743526ce598c71
GoldenCompSci <sam.lukins.compsci@gmail.com>
2018-01-21 21:02:09 -0600

    Missing brace and spaces

--- UAHBotV1.2/LogicHandler.java
+++ UAHBotV1.2/LogicHandler.java
@@ -6,7 +6,7 @@ class LogicHandler {
 	static Unit[] rockets;
 	static int factoryGoal = 10;
 	
-	public static void initialize(GameController gc){
+	public static void initialize(GameController gc) {
 		//initialize Pathing
 		Path.initializePathing(gc);
 		
@@ -26,15 +26,16 @@ class LogicHandler {
 		Worker.productionType = UnitType.Factory;
 	}
 	
-	public static void process(GameController gc){
+	public static void process(GameController gc) {
 		//initialize count of all units
 		Utilities.countUnits(Player.units);
 		
-		if(!escaping && gc.round() >= 720){
+		if(!escaping && gc.round() >= 720) {
 			startEscaping(gc);
+		}
 	}
 	
-	public static void startEscaping(GameController gc){
+	public static void startEscaping(GameController gc) {
 		//set escaping to true
 		escaping = true;
 		
@@ -42,11 +43,11 @@ class LogicHandler {
 		getRocketLocations(gc);
 	}
 	
-	public static void getRocketLocations(GameController gc){
+	public static void getRocketLocations(GameController gc) {
 		rockets = new Unit[Player.numRockets];
 		int i = 0;
-		for(long j = 0; j < Player.units.size(); j++){
-			if(Player.units.get(j).unitType() == UnitType.Rocket){
+		for(long j = 0; j < Player.units.size(); j++) {
+			if(Player.units.get(j).unitType() == UnitType.Rocket) {
 				rockets[i] = Player.units.get(j);
 				i++;
 			}
