0e46ba2741423b44c2a9814dbe0c4b64c83a0326
GoldenCompSci <sam.lukins.compsci@gmail.com>
2018-01-21 18:31:33 -0600

    Base healer off of UAHUnit

--- UAHBotV1.2/Healer.java
+++ UAHBotV1.2/Healer.java
@@ -1,11 +1,9 @@
 import bc.*;
 
-class Healer {
-	public static boolean canProcess(Unit unit) {
-		if(unit.unitType() == UnitType.Healer) {
-			return true;
-		}
-		return false;
+class Healer extends UAHUnit{
+	
+	public Healer(Unit unit, GameController gc) {
+		super(unit, gc);
 	}
 	
 	public static void process(Unit unit, GameController gc) {
