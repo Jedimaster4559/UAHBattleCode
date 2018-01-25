15f7995e16ac2666fe2daed07fe883011db69869
Jedimaster4559 <nbsolomon4559@gmail.com>
2018-01-24 18:45:15 -0600

    added better comments

--- UAHBotV2.0.0/Mage.java
+++ UAHBotV2.0.0/Mage.java
@@ -7,25 +7,26 @@ class Mage extends MobileUnit{
 	}
 	
 	public void process() {
-        
+        	//Do not process if we are not on Earth or Mars
 		if (!unit.location().isOnMap()) {
 			return;
 		}
 		
+		//Attempt to escape/move to the nearest rocket if that is something currently important
 		if (LogicHandler.escaping && unit.movementHeat() < 10) {
 			Utilities.moveTowardNearestRocket(unit, gc);
 		}
 		
-		if (!Player.peaceful) {
+		if (!Player.peaceful) {						//Peaceful catch for debugging purposes
 			if(unit.attackHeat() < 10){
-				Utilities.senseAndAttackInRange(unit, gc);
+				Utilities.senseAndAttackInRange(unit, gc);	//attack an enemy if possible
 			}
 			if(unit.movementHeat() < 10){
-				Utilities.moveToNearestEnemy(unit, gc);
+				Utilities.moveToNearestEnemy(unit, gc);		//move toward an enemy if possible
 			}
 		}
-		if(unit.movementHeat() < 10){
-			Utilities.moveRandomDirection(unit, gc);
+		if(unit.movementHeat() < 10){				
+			Utilities.moveRandomDirection(unit, gc);		//move a random direction if possible
 		}
 	}	
 }
