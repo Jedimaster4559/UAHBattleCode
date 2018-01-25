a065d2dd4fde2cdba7ba141a3e1692df44f8aa9c
Jedimaster4559 <nbsolomon4559@gmail.com>
2018-01-24 18:47:09 -0600

    Added more comments

--- UAHBotV2.0.0/MobileUnit.java
+++ UAHBotV2.0.0/MobileUnit.java
@@ -2,17 +2,17 @@ import bc.*;
 
 abstract class MobileUnit extends UAHUnit {
 	
-	protected MapLocation currentLocation;
-	protected MapLocation dest;
+	protected MapLocation currentLocation;	//The current location of a mobile unit
+	protected MapLocation dest;		//The destination of a mobile unit
 	
 	public MobileUnit(Unit unit, GameController gc) {
 		super(unit, gc);
-		if (unit.location().isOnMap()) {
+		if (unit.location().isOnMap()) {				//attempts to set the bots starting location
 			currentLocation = unit.location().mapLocation();
 		} else {
 			currentLocation = null;
 		}
-		dest = null;
+		dest = null;							//bot has no destination when it is first created
 	}
 	
 	public MapLocation getCurrentLocation() {
