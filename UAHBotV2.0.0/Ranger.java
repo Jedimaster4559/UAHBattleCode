f4dc3bf9c34986cab710769515ff48af4c23e409
Jedimaster4559 <nbsolomon4559@gmail.com>
2018-01-24 19:00:08 -0600

    Added better comments

--- UAHBotV2.0.0/Ranger.java
+++ UAHBotV2.0.0/Ranger.java
@@ -7,25 +7,25 @@ class Ranger extends MobileUnit{
 	}
 	
 	public void process() {
-		
+		//if bot is not on map, do not process
 		if (!unit.location().isOnMap()) {
 			return;
 		}
 		
-		if (LogicHandler.escaping && unit.movementHeat() < 10) {
-			Utilities.moveTowardNearestRocket(unit, gc);
+		if (LogicHandler.escaping && unit.movementHeat() < 10) {	//if we should be escaping to Mars, attempt to 
+			Utilities.moveTowardNearestRocket(unit, gc);		//do so
 		}
 		
-		if (!Player.peaceful) {
+		if (!Player.peaceful) {						//peaceful toggle for debugging purposes
 			if(unit.attackHeat() < 10){
-				Utilities.senseAndAttackInRange(unit, gc);
+				Utilities.senseAndAttackInRange(unit, gc);	//attack enemy if possible
 			}
 			if(unit.movementHeat() < 10){
-				Utilities.moveToNearestEnemy(unit, gc);
+				Utilities.moveToNearestEnemy(unit, gc);		//move toward an enemy if possible
 			}
 		}
 		if(unit.movementHeat() < 10){
-			Utilities.moveRandomDirection(unit, gc);
+			Utilities.moveRandomDirection(unit, gc);		//move a random direction if possible
 		}
 	}
 
