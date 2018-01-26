ae1cee0acfe571aa351ccfb97a41e446e9983d17
R-Womack <raw0025@uah.edu>
2018-01-26 17:00:00 -0600

    changed file name
    due to changed directory

---
+++ UAHBotV2.0.0/Factory.java
@@ -0,0 +1,107 @@
+import bc.*;
+
+class Factory extends Structure {
+	
+	//By default when a factory is created it is still under construction.
+	private boolean built = false;
+	private int workerGoal = 10; // abstraction of the number of workers we want for flexibility.
+	
+	private final int knightRatio = 4;	// ratios of each combat unit to keep overall balance
+	private final int rangerRatio = 2;	// between the number of each in play.
+	private final int mageRatio = 1;
+	
+	
+	
+	public Factory(Unit unit, GameController gc) {
+		super(unit, gc);
+		built = false;
+	}
+
+	
+	public void process() {
+		
+		//check to determine if the factory has been built
+		if (!built) {
+			if (unit.structureIsBuilt() == 1) {
+				built = true;	//true if it does
+			} else {
+				return;		//returns and does not process if the factor has not been built yet (prevents errors)
+			}
+		}
+		
+		//Attempts to unload all bots
+		if(unit.structureGarrison().size() > 0)
+		{
+			Direction[] directions = Direction.values();				//get all directions (improve this later)
+			for (Direction direction : directions) {					//Loop through all of the directions
+				if (gc.canUnload(unit.id(), direction)) {				//Check if the bot can unload in a given dir direction
+					int unloadId = unit.structureGarrison().get(0);			
+					Unit unloadUnit = gc.unit(unloadId);				//set important variables for unload
+					UnitType unloadType = unloadUnit.unitType();
+					switch (unloadType) {						//Go through all unit types and create a new object
+						case Worker:							//of the type of unit we plan to unload
+							Worker newWorker = new Worker(unloadUnit, gc);	
+							Player.newUnits.add(newWorker);
+							break;
+						case Knight:
+							Knight newKnight = new Knight(unloadUnit, gc);
+							Player.newUnits.add(newKnight);
+							break;
+						case Ranger:
+							Ranger newRanger = new Ranger(unloadUnit, gc);
+							Player.newUnits.add(newRanger);
+							break;
+						case Mage:
+							Mage newMage = new Mage(unloadUnit, gc);
+							Player.newUnits.add(newMage);
+							break;
+						case Healer:
+							Healer newHealer = new Healer(unloadUnit, gc);
+							Player.newUnits.add(newHealer);
+							break;
+					}
+						
+					gc.unload(unit.id(), direction);				//unload unit in the given direction
+					if(unit.structureGarrison().size() == 0){
+						break;							//if the factory happens to be empty now, stop unloading
+					}									//if not, keep trying different directions
+				}
+			}
+		}
+		
+		if(unit.isFactoryProducing() == 0) {				//create new unit if the factory is not already working on one
+			UnitType unitCreateType = decideUnitType();		//decide the unit type of said unit
+			if(gc.canProduceRobot(unit.id(), unitCreateType)){	
+				gc.produceRobot(unit.id(), unitCreateType);	//create the unit if it is possible to do so
+			}
+		}
+
+	}
+	
+	public UnitType decideUnitType() {
+		
+		private int knightMultiplier = Player.numKnights / knightRatio; // multipliers help determine
+		private int rangerMultiplier = Player.numRangers / rangerRatio; // what unit needs to be produced.
+		private int mageMultiplier = Player.numMages / mageRatio;
+	
+		private int[] cmpArray = {knightMultiplier, rangerMultiplier, mageMultiplier}; // array of multipliers.
+		private int lowest = knightMultiplier;
+		for(int i = 0; i < cmpArray.size; i++) {
+			if(cmpArray[i] < lowest){
+				lowest = cmpArray[i];	// iterates through the array to find the lowest multiplier.
+			}
+		}
+			
+		if ((Player.numWorkers < workerGoal)&&(Player.numWorkers <= (2*Player.numKnights))) {	// initial expression to determine whether		
+			return UnitType.Worker;																// to produce more workers.
+		} else if (knightMultiplier == lowest) {	// decides unit based on lowest multiplier.
+			return UnitType.Knight;					// ideally, all multipliers will be equal.
+		} else if (rangerMultiplier == lowest) {
+			return UnitType.Knight;
+		} else if (mageMultiplier == lowest) {
+			return UnitType.Mage;
+		} else {
+			return UnitType.Knight;		// default unit
+		}
+	}
+}
---
+++ UAHBotV2.0.0/Player.java
@@ -0,0 +1,134 @@
+import bc.*;
+import java.util.ArrayList;
+import java.lang.Long;
+import java.util.Random;
+
+public class Player {
+	
+
+	//create all variable that might be used throughout run
+	static Random rand;			//Random to be used when needed
+	static Direction[] directions;		//Array of all directions (this can be improved with an array without Direction.Center)
+	static GameController gc;		//the game controller so it is more available to us
+	
+	//counters for all units
+	static int numFactories;
+	static int numWorkers;
+	static int numKnights;
+	static int numMages;
+	static int numRangers;
+	static int numHealers;
+	static int numRockets;
+	
+	// set variables to be used for karboniteGoal
+	static final double kgMultiplier = 1.2;			// this multiplier helps ensure there is additional karbonite to use for unit production in the late game
+	static final int rocketCost = 150;				// the literal production cost of karbonite, taken from the battlecode game specs
+	
+	//Set some strategy goals
+	static int karboniteGoal = rocketGoal * rocketCost * kgMultiplier;
+	static int rocketGoal = (double)(math.ceil());
+	static int factoryGoal = 5;
+	
+	
+	static VecUnit units;						//a VecUnit of all of our units (this may no longer be necessary)
+	static boolean peaceful = false;				//Peaceful toggle for bot
+	static ArrayList<UAHUnit> UAHUnits = new ArrayList<UAHUnit>();	//List of all of our units
+	static ArrayList<UAHUnit> newUnits = new ArrayList<UAHUnit>();	//list of all units created this turn
+	static ArrayList<UAHUnit> deadUnits = new ArrayList<UAHUnit>();	//list of all units that died this turn
+
+		
+	public static void main(String[] args) {
+	
+		//Add a Game Controller
+		gc = new GameController();
+		
+		//Seed Randomizer for debugging purposes
+		rand = new Random();
+		//rand.setSeed(1337);
+
+		//Grab the enemy team locations
+		Utilities.findEnemyTeam(gc);
+
+		//Create and Array of all Directions a bot can travel (Maybe change to array without Direction.Center?)
+		directions = Direction.values();
+		
+		//initialize logic handler
+		LogicHandler.initialize(gc);
+
+		//get a list of all our units at game start
+		units = gc.myUnits();
+		
+		//add all inital units to units ArrayList
+		for (int i = 0; i < units.size(); i++) {
+			UAHUnits.add(new Worker(units.get(i), gc));
+		}
+		
+		
+		//loop through all units and process their turn
+		while (true){
+			//Determine if we are on earth or on mars to improve the processing of the bot
+			if(gc.planet() == Planet.Mars && gc.round() > 700){
+				//find rockets when they land and at them to units list
+				Utilities.verifyList(gc);
+				UAHUnits.addAll(newUnits);
+				deadUnits.clear();	//should this be newUnits.clear? Could be causing us some process time issues
+				
+				//try to run all units this turn
+				try {
+					for (UAHUnit unit : UAHUnits) {		//Loop through all of our units
+						if (unit.isAlive()) {
+							unit.preProcess();	//Preprocess the unit (determines if the bot still exists)
+							unit.process();		//Allow the unit to process its turn
+						}
+						
+					}
+					
+					//process any unit changes from this turn
+					UAHUnits.removeAll(deadUnits);
+					UAHUnits.addAll(newUnits);
+					
+					//clear changes to be used next turn
+					newUnits.clear();
+					deadUnits.clear();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			else{
+				//Process Logic
+				LogicHandler.process(gc);
+				
+				//try to run all units this turn
+				try {
+					for (UAHUnit unit : UAHUnits) {		//Loop through all of our units
+						if (unit.isAlive()) {
+							unit.preProcess();	//Preprocess the unit (deterimnes if it is alive)
+							unit.process();		//process the unit's actions
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				//add all unit changes to main list
+				UAHUnits.addAll(newUnits);
+				UAHUnits.removeAll(deadUnits);
+				
+				//run any new units
+				for (UAHUnit unit : newUnits) {
+					unit.process();
+				}
+				
+				//clear unit changes
+				newUnits.clear();
+				deadUnits.clear();
+				
+			}
+			
+			//proceed to next turn
+			gc.nextTurn();
+        	}
+		
+	}
+
+}
